b6c53dd9651c2ffeab615d9296d4af5568323cc8
ersinisgor <ersinisgor@hotmail.com>
2025-05-14 14:04:40 +0300

    feat: Add BookController

---
+++ Controllers/BookController.cs
@@ -0,0 +1,14 @@
+﻿using BookCatalog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookCatalog.Controllers
+{
+    public class BookController(ApplicationDbContext _context) : Controller
+    {
+        public IActionResult Index()
+        {
+            var books = _context.Books.ToList();
+            return View(books);
+        }
+    }
+}
