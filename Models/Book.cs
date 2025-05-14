22eb296405c6ce85c6b94eb4e002a1c0837fd2e9
ersinisgor <ersinisgor@hotmail.com>
2025-05-14 05:06:17 +0300

    feat: Add data validation to Book model

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,11 +1,17 @@
-﻿namespace BookCatalog.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookCatalog.Models
 {
     public class Book
     {
         public int Id { get; set; } 
+        [Required(ErrorMessage = "Title is required.")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Authro is required.")]
         public string Author { get; set; }
+        [Required(ErrorMessage = "Genre is required.")]
         public string Genre { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Page count must be at least one.")]
         public int PageCount { get; set; }
     }
 }
