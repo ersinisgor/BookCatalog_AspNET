a08fbd1ea9325b4d68a97e63f4e590859728e817
ersinisgor <ersinisgor@hotmail.com>
2025-05-14 05:04:38 +0300

    feat: Add ApplicationDbContext with Book entity

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BookCatalog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookCatalog.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public virtual DbSet<Book> Books { get; set; }
+    }
+}
