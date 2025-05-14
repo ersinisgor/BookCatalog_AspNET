b3ad3e0d1f740ae288304ba9d3728d32de3eeb7e
ersinisgor <ersinisgor@hotmail.com>
2025-05-14 06:26:37 +0300

    refactor: Use fully qualified DbContext name in DI registration

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<BookCatalog.Data.ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 Log.Logger = new LoggerConfiguration()
