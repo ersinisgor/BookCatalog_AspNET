2542e8d8450cd3bf659656e830c0ff40611093a3
ersinisgor <ersinisgor@hotmail.com>
2025-05-15 09:58:30 +0300

    feat: Implement FluentValidation for Book model

---
+++ Validators/BookValidator.cs
@@ -0,0 +1,27 @@
+﻿using BookCatalog.Models;
+using FluentValidation;
+
+namespace BookCatalog.Validations
+{
+    public class BookValidator: AbstractValidator<Book>
+    {
+        public BookValidator() {
+            RuleFor(book => book.Title)
+                .NotNull()
+                .NotEmpty()
+                .Length(1,50);
+            RuleFor(book => book.Author)
+                .NotNull()
+                .NotEmpty()
+                .Length(1,50);
+            RuleFor(book => book.Genre)
+                .NotNull()
+                .NotEmpty()
+                .Length(1,50);
+            RuleFor(book => book.PageCount)
+                .NotNull()
+                .GreaterThan(0)
+                .WithName("Page Count");
+        }
+    }
+}
