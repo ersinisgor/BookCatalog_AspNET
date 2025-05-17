82c982d3ae49872c0f1d8d7ea7457916b37fb940
ersinisgor <ersinisgor@hotmail.com>
2025-05-17 12:38:19 +0300

    feat: Add BookDTO class for structured book data transfer

---
+++ Shared/DTOs/BookDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace BookCatalog.Shared.DTOs
+{
+    public class BookDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int PageCount { get; set; }
+    }
+}
