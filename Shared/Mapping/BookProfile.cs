2fd744b5c19c3b102def070788153400431c22df
ersinisgor <ersinisgor@hotmail.com>
2025-05-17 12:36:56 +0300

    feat: Add AutoMapper profile for book entities

---
+++ Shared/Mapping/BookProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BookCatalog.Domain.Entities;
+using BookCatalog.Shared.DTOs;
+
+namespace BookCatalog.Shared.Mapping
+{
+    public class BookProfile: Profile
+    {
+        public BookProfile()
+        {
+            CreateMap<CreateBookDTO, Book>();
+            CreateMap<Book, BookDTO>();
+        }
+    }
+}
