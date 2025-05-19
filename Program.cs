258e3269799fedffc6bef03ded19b2e7b05c278c
ersinisgor <ersinisgor@hotmail.com>
2025-05-19 05:22:01 +0300

    refactor: Enhance logging configuration and move Serilog configuration to use appsettings.json

--- Program.cs
+++ Program.cs
@@ -5,16 +5,19 @@ using BookCatalog.Infrastructure.Data;
 using BookCatalog.Infrastructure.Repositories;
 using BookCatalog.Application.Services;
 using BookCatalog.Shared.Mapping;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc.Razor;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews().AddRazorOptions(options =>
-{
-    options.ViewLocationFormats.Clear();
-    options.ViewLocationFormats.Add("/Presentation/Views/{1}/{0}.cshtml");
-    options.ViewLocationFormats.Add("/Presentation/Views/Shared/{0}.cshtml");
-});
+builder.Services.AddControllersWithViews()
+    .AddRazorOptions(options =>
+    {
+        options.ViewLocationFormats.Clear();
+        options.ViewLocationFormats.Add("/Presentation/Views/{1}/{0}.cshtml");
+        options.ViewLocationFormats.Add("/Presentation/Views/Shared/{0}.cshtml");
+    });
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -24,10 +27,8 @@ builder.Services.AddScoped<IBookService, BookService>();
 builder.Services.AddAutoMapper(typeof(BookProfile));
 builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly, includeInternalTypes: true);
 
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.File("Logs/logs.txt", rollingInterval: RollingInterval.Day)
-    .CreateLogger();
-builder.Host.UseSerilog();
+// Serilog configuration from appsettings.json
+builder.Host.UseSerilog((ctx, lc) => lc.ReadFrom.Configuration(ctx.Configuration));
 
 var app = builder.Build();
 
@@ -35,7 +36,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
